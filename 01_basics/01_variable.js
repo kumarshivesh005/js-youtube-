df7d6347d4a94e407477d7c3811c0363e9ef7e47
kumarshivesh005 <kumarshiveshstm17@gmail.com>
2026-03-17 13:56:17 +0000

    datatype

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,16 @@
+const accountId=14445;
+let accountEmail="shivesh@google.com"
+var accountPassword="Shivesh"
+accountCity="jaipur";
+
+// accountId=65437;//const value cannot be change
+
+/*
+var cannot be used bcz it has issue in functional scope and block scope
+
+*/
+accountEmail="lalan@gmail.com"
+accountPassword="Lalan"
+accountCity="sitamarhi"
+
+console.table([accountEmail,accountId,accountPassword,accountCity]);
---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,20 @@
+// two scripts: mdn and ecma
+
+"use strict";//use newer version in js
+
+// alert(3+3);//bcz we are using nodejs not console of browser
+
+let name="Shhivesh"//sstring
+let age=18//number
+let bigint=56n;//bigint for bigger number
+let isLoggedin=true //boolean
+// null;-it is also a value in the js 
+// undefined:-value is not given to them
+// symbol:-used for unique feature
+
+
+// object:
+
+// typeof (null ) is object 
+console.log(typeof(null));
+console.log(typeof(undefined));
