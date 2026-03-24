a22c404f7c9643f8b0d61dbb6eebd748044874df
kumarshivesh005 <kumarshiveshstm17@gmail.com>
2026-03-24 13:38:55 +0000

    add clock locally

---
+++ 07_project/03_project /chaiaurcode.js
@@ -0,0 +1,10 @@
+const clock=document.getElementById('clock');
+// let date=new Date();
+// clock.innerHTML=date.toLocaleTimeString();
+// console.log(date.toLocaleTimeString())
+
+setInterval(function(){
+    let date=new Date();
+    clock.innerHTML=date.toLocaleTimeString();
+
+},4000)
